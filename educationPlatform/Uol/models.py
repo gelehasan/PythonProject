a43bdc6749a606a0377c4b0cdbedbddf02b6770a
Gele hasan <gellehussen@gmail.com>
2024-06-11 20:22:43 +0200

    added table modules§

--- educationPlatform/Uol/models.py
+++ educationPlatform/Uol/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
+ 
 # Create your models here.
+
+class User(models.Model):
+    id= models.AutoField(primary_key=True) 
+    userName= models.CharField(max_length=256, null=False, blank=False)
+    email=  models.CharField(max_length=256, null=False, blank=False)
+    role =  models.CharField(max_length=256, null=False, blank=False)
+    joinDate = models.DateField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.userName
+
+class Course(models.Model):
+     id= models.AutoField(primary_key=True) 
+     title= models.CharField(max_length=256, null=False, blank=False)
+     description = models.TextField(null=False, blank=False)
+     category = models.CharField(max_length=256, null=False, blank=False)
+     duration = models.IntegerField(null=False, blank=False)
+     instructor = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+     publishDate = models.DateField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.title
+
+     
+class Enrollment(models.Model):
+    id = models.AutoField(primary_key=True) 
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    course= models.ForeignKey(Course,on_delete=models.CASCADE)
+    enrollmentDate = models.DateField(auto_now_add=True)
+    progress = models.IntegerField(null=False, blank=False)
+    status = models.CharField(max_length=256, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.user.userName} enrolled in {self.course.title}"
+
+class CourseReview (models.Model):
+    id = models.AutoField(primary_key=True) 
+    user= models.ForeignKey(User,on_delete=models.CASCADE)
+    course =  models.ForeignKey(Course,on_delete=models.CASCADE)
+    rating = models.IntegerField(null=False, blank=False)
+    comment = models.TextField(null=False, blank=False)
+    ReviewDate= models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user.userName} for {self.course.title} with rating {self.rating}"
