2ecfaa837c7436a48f5f927c8eacd904d0337e71
Gele hasan <gellehussen@gmail.com>
2024-06-17 23:28:01 +0200

    created basic user detail api

---
+++ educationPlatform/Uol/api.py
@@ -0,0 +1,23 @@
+from rest_framework import generics, mixins
+from .models import User, Course, Enrollment, CourseReview
+from .serializers import UserSerializer, CourseSerializer, EnrollmentSerializer, CourseReviewSerializer
+# Basic CRUD views
+class UserDetail(mixins.CreateModelMixin,
+                 mixins.RetrieveModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin,
+                 generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
