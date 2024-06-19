5a29ced5bb5d1effc554d9d49acfe9d432c428a8
Gele hasan <gellehussen@gmail.com>
2024-06-19 23:36:38 +0200

    importing nessecary modules in views

--- educationPlatform/Uol/views.py
+++ educationPlatform/Uol/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import User, Course, Enrollment, CourseReview
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
